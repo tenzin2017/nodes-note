d4070c9a947c849625540a55bc6db6a8cdf0e667
tenzin2017 <tenzinleo@gmail.com>
2018-01-31 19:07:09 -0500

    fs os modules

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 console.log('Starting app.');
 
 const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user);
 
 
-fs.appendFile('greetings.txt', 'Hello World !', function(err){
+fs.appendFile('greetings.txt', `Hello ${user.username}`, function(err){
     if(err){
         console.log("Unable to write to file");
         
