3e2c958b366d3dab463dc3e4fa01d5d333f02424
tenzin2017 <tenzinleo@gmail.com>
2018-01-31 19:01:19 -0500

    my nodes note

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+
+
+fs.appendFile('greetings.txt', 'Hello World !', function(err){
+    if(err){
+        console.log("Unable to write to file");
+        
+    }
+});
