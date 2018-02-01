8a93e72b960b215c80bf26b57979fd3e310f48a1
tenzin2017 <tenzinleo@gmail.com>
2018-01-31 20:02:46 -0500

    installed npm lodash and exports two functions : _.isString and _.uniq"

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-var res = notes.addNote();
-console.log(res);
-
-var sum = notes.sum(3,4);
-console.log('Result:', sum);
+console.log(_.isString(12));
+console.log((_.isString('Lobsang')));
 
 
+var filteredArray = _.uniq(['Lobsang', 1, 2,3,1, 'Lobsang']);
+console.log(filteredArray);
 
-var user = os.userInfo();
+
+
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var sum = notes.sum(3,4);
+// console.log('Result:', sum);
+
+
+
+// var user = os.userInfo();
 // console.log(user);
 
 
